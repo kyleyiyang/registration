7f406fdff0142584301be71f94ef2471394051b7
kyleyiyang <kyleyiyang@gmail.com>
2020-06-14 22:15:29 -0400

    Create modal-authenticate.php

---
+++ modal-authenticate.php
@@ -0,0 +1,2 @@
+In theme editor, DirectoryEngine: modal-authenticate.php (template-js/modal-authenticate.php)
+Line 47, change style = “display:none;” to <form class="signup_form form_modal_style" id="signup_form" style=""> to show the registration part.
