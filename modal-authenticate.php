d01ca41c24788f0067f30f2829d1d7fd3db807db
kyleyiyang <kyleyiyang@gmail.com>
2020-06-15 13:10:00 -0400

    Update modal-authenticate.php

--- modal-authenticate.php
+++ modal-authenticate.php
@@ -1,2 +1,13 @@
 In theme editor, DirectoryEngine: modal-authenticate.php (template-js/modal-authenticate.php)
 Line 47, change style = “display:none;” to <form class="signup_form form_modal_style" id="signup_form" style=""> to show the registration part.
+  
+  Line 56, added: <div class="form-field">
+		        	      <label><?php _e("City", ET_DOMAIN) ?></label>
+		        	      <input type="text" class="text-field city_user" name="user_city" id="user_city" />
+				          </div>
+				          <div class="form-field">
+		        	         <label><?php _e("State", ET_DOMAIN) ?></label>
+                      <input type="text" class="text-field stae_user" name="user_state" id="user_state" />
+				          </div>
+
+
